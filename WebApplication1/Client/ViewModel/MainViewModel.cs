9f026a1962aaaba0b947ade6449d1543e7d8b6e5
MaximeCollot <maxime.collot.69@gmail.com>
2017-10-23 16:59:39 +0200

    Debut client: page home

---
+++ WebApplication1/Client/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
+
+namespace Client.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ICommand BtnPrevious { get; set; }
+        public ICommand BtnHome { get; set; }
+        public ICommand BtnMovies { get; set; }
+        public ICommand BtnAccount { get; set; }
+        public ICommand BtnParameters { get; set; }
+
+        public MainViewModel()
+        {
+            BtnPrevious = new RelayCommand(goToPrevious);
+            BtnHome = new RelayCommand(goToHome);
+            BtnMovies = new RelayCommand(goToMovies);
+            BtnAccount = new RelayCommand(goToAccount);
+            BtnParameters = new RelayCommand(goToParameters);
+        }
+
+        private void goToPrevious()
+        {
+            //Frame myFrame = MainSplitView.Content as Frame;
+            //if (myFrame.CanGoBack)
+            //{
+            //    myFrame.GoBack();
+            //}
+        }
+
+        private void goToHome()
+        {
+
+        }
+
+        private void goToMovies()
+        {
+
+        }
+
+        private void goToAccount()
+        {
+
+        }
+
+        private void goToParameters()
+        {
+
+        }
+    }
+}
